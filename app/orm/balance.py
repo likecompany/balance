1a3c9d2f994d8f6943e8c164c955e09cfc0106c3
toymaj <sermed512@gmail.com>
2023-08-13 12:56:04 +0300

    refactor orm

--- app/orm/balance.py
+++ app/orm/balance.py
@@ -6,6 +6,5 @@ from .core import ORMModel, types
 
 
 class BalanceModel(ORMModel):
-    id: Mapped[types.BigInt] = mapped_column(unique=True, primary_key=True)
     user_id: Mapped[types.BigInt] = mapped_column(unique=True)
     balance: Mapped[types.BigInt] = mapped_column(default=15000)
